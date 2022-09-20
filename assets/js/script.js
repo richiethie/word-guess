3e325ff9fb827b0d1e5bdf0270a4a9774debbe8b
Richie Thiesfeldt <richiethie@gmail.com>
2022-09-19 21:45:51 -0500

    Keydown listener, finish renderCharacters()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,42 @@
 var startBtn = document.querySelector('.start-button')
 var wordsBlanksEl = document.querySelector('.word-blanks') 
 
+var validChars = "abcdefghijklmnopqrstuvwxyz"
 // var for array of words
 var words = ["javascript", "variable", "function", "object", "python", "localstorage", "timeout", "interval"]
 var word
+var guessedCharacters = []
 // score variable
 // time left variable
+
+//function for when key is pressed
+function handleKeyDown(event) {
+    console.log(event.key)
+    if (validChars.includes(event.key)) {
+        // keep track of character that was guessed, var guessedCharacters = []
+        // push key press into guessedCharacters array
+        guessedCharacters.push(event.key)
+        // re-render wordBlanks.textContent
+        renderCharacters()
+    }
+}
 
 // reset the word-blanks, an _ for each letter of the new word
 function renderCharacters() {
     // var to hold a new string
     var str = ""
     for (var i = 0; i < word.length; i++) {
+        var letter = word[i]
         //  if we have guessed the character that we are currently on
+        if (guessedCharacters.includes(letter)) {
             // add the character into str
-        // else
+            str += letter + " "
+        }
+        else {
             // add an _ into str
             str += "_ "
-            console.log(str)
+
+        }
     }
     // set textContent of wordBlanksEl to be str
     wordsBlanksEl.textContent = str.trim()
@@ -32,3 +51,6 @@ function startRound() {
 }
 //addEventListener to start button (click)
 startBtn.addEventListener('click', startRound)
+
+//addEventListener to detect typing in _
+document.addEventListener('keydown', handleKeyDown) //function for when key is pressed
